2ab8623fab71929948e8a5c17aaf50aba4b8a057
Shane McDonough <shanemcdman@gmail.com>
2020-12-27 00:14:40 -0800

    Do not round in grid_to_window

--- src/DrawingWindow.cpp
+++ src/DrawingWindow.cpp
@@ -17,8 +17,8 @@ void DrawingWindow::draw_cursor(){
 
 Vector2 DrawingWindow::grid_to_window(Vector2 pos){
     return {
-        roundf(pos.x * cell_size.x),
-        roundf(pos.y * cell_size.y),
+        pos.x * cell_size.x,
+        pos.y * cell_size.y,
     };
 }
 
@@ -34,18 +34,11 @@ DrawingWindow::DrawingWindow(Vector2 size):window_size(size){}
 void DrawingWindow::run(){
     InitWindow(window_size.x, window_size.y, "Drawing Window");
     SetTargetFPS(30);
-    Circle l;
-    l.add_vertex({500, 500});
-    l.add_vertex({600, 600});
-    // shapes.push_back(Line());
-    // shapes[0].add_vertex({100, 100});
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(BLACK);
         draw_grid();
         draw_cursor();
-        // shapes[0].draw();
-        l.draw();
         EndDrawing();
     }
     CloseWindow();
