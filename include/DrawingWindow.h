daa4b96253e759c617ed2661db72972ce39a51fd
Shane McDonough <shanemcdman@gmail.com>
2020-12-27 00:39:28 -0800

    Start using pan_offset

--- include/DrawingWindow.h
+++ include/DrawingWindow.h
@@ -8,13 +8,15 @@
 class DrawingWindow{
 private:
     Vector2 window_size;
-    Vector2 cell_size{40,40};
+    Vector2 cell_size{40, 40};
+    Vector2 pan_offset{1000, 1000};
     char* window_title;
     std::vector<Shape> shapes;
     void draw_grid();
     void draw_cursor();
     Vector2 grid_to_window(Vector2 pos);
     Vector2 window_to_grid(Vector2 pos);
+    Vector2 get_mouse_grid();
 public:
     DrawingWindow(Vector2 _window_size);
     void run();
--- src/DrawingWindow.cpp
+++ src/DrawingWindow.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include<algorithm>
 #include<math.h>
 #include"DrawingWindow.h"
@@ -9,27 +10,35 @@ void DrawingWindow::draw_grid(){
         DrawLine(i, 0, i, window_size.y, {30, 30, 30, 255});
     for(int i = 0; i < window_size.y; i += cell_size.y)
         DrawLine(0, i, window_size.x, i, {30, 30, 30, 255});
+    DrawLine(0, pan_offset.y, window_size.x, pan_offset.y, GRAY);
+    DrawLine(pan_offset.x, 0, pan_offset.x, window_size.y, GRAY);
 }
 
 void DrawingWindow::draw_cursor(){
-    DrawCircleV(grid_to_window(window_to_grid(GetMousePosition())), 10, {0, 220, 0, 100});
+    DrawCircleV(get_mouse_grid(), 10, {0, 220, 0, 100});
 }
 
 Vector2 DrawingWindow::grid_to_window(Vector2 pos){
     return {
-        pos.x * cell_size.x,
-        pos.y * cell_size.y,
+        pos.x * cell_size.x + pan_offset.x,
+        pos.y * cell_size.y + pan_offset.y,
     };
 }
 
 Vector2 DrawingWindow::window_to_grid(Vector2 pos){
     return {
-        roundf(pos.x / cell_size.x),
-        roundf(pos.y / cell_size.y),
+        roundf((pos.x - pan_offset.x) / cell_size.x),
+        roundf((pos.y - pan_offset.y) / cell_size.y),
     };
 }
 
-DrawingWindow::DrawingWindow(Vector2 size):window_size(size){}
+Vector2 DrawingWindow::get_mouse_grid(){
+    return grid_to_window(window_to_grid(GetMousePosition()));
+}
+
+DrawingWindow::DrawingWindow(Vector2 size):window_size(size){
+    pan_offset = {size.x / 2, size.y / 2};
+}
 
 void DrawingWindow::run(){
     InitWindow(window_size.x, window_size.y, "Drawing Window");
